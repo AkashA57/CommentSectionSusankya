ba7ca237425ffa1d3d983d5d09a89f141e11246f
AkashA57 <78446970+AkashA57@users.noreply.github.com>
2022-04-18 10:24:50 -0400

    Update index.js

--- Comment CookBook1/index.js
+++ Comment CookBook1/index.js
@@ -40,6 +40,9 @@ function addComment(ev) {
  const deleteButton = document.createElement('button');
  deleteButton.innerHTML = 'Delete';
  deleteButton.className = 'deleteComment';
+ const editButton = document.createElement('button');
+ editButton.innerHTML = 'Edit';
+ editButton.className = 'editComment';
  if(hasClass(ev.target.parentElement.parentElement, 'container')) {
      const wrapDiv = document.createElement('div');
      wrapDiv.style.boxShadow = "5px 5px 10px black";
@@ -49,16 +52,16 @@ function addComment(ev) {
      document.getElementById('comment').value = '';
      textBox.innerHTML = commentText;
      textBox.style.backgroundColor = "aliceblue";
-     wrapDiv.append(author, textBox, replyButton, likeButton, deleteButton);
+     wrapDiv.append(author, textBox, replyButton, likeButton, deleteButton, editButton);
      commentContainer.appendChild(wrapDiv);
  } else {
      wrapDiv = ev.target.parentElement;
-     wrapDiv.style.boxShadow = "10px 20px 30px blue";
+     // wrapDiv.style.boxShadow = "10px 20px 30px blue";
      commentText = ev.target.parentElement.firstElementChild.value;
      textBox.innerHTML = commentText;
      textBox.style.backgroundColor = "cornsilk";
      wrapDiv.innerHTML = '';
-     wrapDiv.append(author, textBox, replyButton, likeButton, deleteButton);
+     wrapDiv.append(author, textBox, replyButton, likeButton, deleteButton, editButton);
  }
  setOnLocalStorage();
 }
@@ -67,24 +70,31 @@ function setOnLocalStorage () {
  localStorage.setItem('template', document.getElementById('allComments').innerHTML);
 }
 function hasClass(elem, className) {
+ console.log(elem.className.split(' ').indexOf(className));
  return elem.className.split(' ').indexOf(className) > -1;
 }
 document.getElementById('allComments').addEventListener('click', function (e) {
  if (hasClass(e.target, 'reply')) {
-     const parentDiv = e.target.parentElement;
-     const wrapDiv = document.createElement('div');
-     wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
-     wrapDiv.className = 'wrapper';
-     const textArea = document.createElement('textarea');
-     textArea.style.marginRight = '20px';
-     const addButton = document.createElement('button');
-     addButton.className = 'addReply';
-     addButton.innerHTML = 'Add';
-     const cancelButton = document.createElement('button');
-     cancelButton.innerHTML = 'Cancel';
-     cancelButton.className='cancelReply';
-     wrapDiv.append(textArea, addButton, cancelButton);
-     parentDiv.appendChild(wrapDiv);
+     if(e.target.parentElement.querySelector(".textarea")!=null) {
+      e.target.parentElement.querySelector(".textarea").parentElement.remove();
+     }
+     else {
+      const parentDiv = e.target.parentElement;
+      const wrapDiv = document.createElement('div');
+      wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
+      wrapDiv.className = 'wrapper';
+      const textArea = document.createElement('textarea');
+      textArea.className = 'textarea';
+      textArea.style.marginRight = '20px';
+      const addButton = document.createElement('button');
+      addButton.className = 'addReply';
+      addButton.innerHTML = 'Add';
+      const cancelButton = document.createElement('button');
+      cancelButton.innerHTML = 'Cancel';
+      cancelButton.className='cancelReply';
+      wrapDiv.append(textArea, addButton, cancelButton);
+      parentDiv.appendChild(wrapDiv);
+     }
 
  } else if(hasClass(e.target, 'addReply')) {
      addComment(e);
